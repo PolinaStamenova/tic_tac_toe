57f316e122b9dba48752805717019780e1a65d0b
PolinaStamenova <polina.stamenova@gmail.com>
2021-10-17 19:26:47 +0300

    Add choose_number method

--- lib/main.rb
+++ lib/main.rb
@@ -1,14 +1,16 @@
 require_relative "table"
 require_relative "player"
 require_relative "create_player"
+require_relative "choose_number"
 
 class App
 
-  attr_reader :table, :create_player
+  attr_reader :table, :create_player, :choose_number
 
   def initialize
     @table = Table.new
     @create_player = CreatePlayer.new
+    @choose_number = ChooseNumber.new
   end
 
   def run
@@ -18,6 +20,8 @@ class App
     @create_player.create_player
     puts
     @table.display_table
+    @choose_number.choose_number
+    # p @choose_number.number
     # @table.replace_symbol("2", "x")
   end
 end
